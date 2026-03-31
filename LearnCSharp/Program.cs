624f3db4ff70161dd4ca30e0fbfad4aee310a550
denny <denycam9@gmail.com>
2026-03-31 13:02:05 +0700

    style: add formatting with format docs

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,24 +1,14 @@
 ﻿var MAX_ITERATION = 15;
 
-for (int i = 1; i <= MAX_ITERATION; i++)
-{
-
-    if (i % 3 == 0)
-    {
+for (int i = 1; i <= MAX_ITERATION; i++) {
+    if (i % 3 == 0) {
         Console.Write("Foo, ");
-    }
-    else if (i % 5 == 0)
-    {
+    } else if (i % 5 == 0) {
         Console.Write("Bar, ");
-    }
-    else if (i % 3 == 0 && i % 5 == 0)
-    {
+    } else if (i % 3 == 0 && i % 5 == 0) {
         Console.Write("FooBar, ");
-    }
-    else
-    {
+    } else {
         Console.Write(i + ", ");
     }
-
 }
 
