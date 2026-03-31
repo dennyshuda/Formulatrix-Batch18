2106ae0dff48aee91363ba43fad31a5f91d3b69f
denny <denycam9@gmail.com>
2026-03-31 11:11:22 +0700

    feat: loop logic exercise

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var MAX_ITERATION = 15;
+
+for (int i = 1; i <= MAX_ITERATION; i++)
+{
+
+    if (i % 3 == 0)
+    {
+        Console.Write("Foo, ");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("Bar, ");
+    }
+    else if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("FooBar, ");
+    }
+    else
+    {
+        Console.Write(i + ", ");
+    }
+
+}
+
