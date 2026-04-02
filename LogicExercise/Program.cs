75bb16a68cd5ea937cc9c6b8b2494021cf5e4483
denny <denycam9@gmail.com>
2026-04-02 10:09:13 +0700

    refactor: move exercise to diffrent folder

---
+++ LogicExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿const int MAX_ITERATION = 15;
+
+for (int i = 1; i <= MAX_ITERATION; i++) {
+    if (i % 3 == 0) {
+        Console.Write("Foo, ");
+    } else if (i % 5 == 0) {
+        Console.Write("Bar, ");
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        Console.Write("FooBar, ");
+    } else {
+        Console.Write(i + ", ");
+    }
+}
