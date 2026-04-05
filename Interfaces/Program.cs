e90c0fc4470c88759be5af054eff5ed6eec831af
Denny Setyawan Huda <denycam9@gmail.com>
2026-04-05 19:19:05 +0700

    feat: learn interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,9 @@
+﻿using Interfaces;
+
+Countdown countdown = new Countdown();
+
+Console.WriteLine("Current: " + countdown.Current);
+countdown.MoveNext();
+Console.WriteLine("Current: " + countdown.Current);
+countdown.MoveNext();
+Console.WriteLine("Current: " + countdown.Current);
