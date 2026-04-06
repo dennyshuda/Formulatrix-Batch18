279d5732a5e89453f6645da5ebf842a73efed94a
denny <denycam9@gmail.com>
2026-04-06 08:19:14 +0700

    fix: adjust the order of the conditions in the sequence.

--- LogicExercise/Program.cs
+++ LogicExercise/Program.cs
@@ -1,12 +1,12 @@
-﻿const int MAX_ITERATION = 15;
+﻿const int MAX_ITERATION = 50;
 
 for (int i = 1; i <= MAX_ITERATION; i++) {
-    if (i % 3 == 0) {
-        Console.Write("Foo, ");
+    if (i % 3 == 0 && i % 5 == 0) {
+        Console.Write("FooBar, ");
     } else if (i % 5 == 0) {
         Console.Write("Bar, ");
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        Console.Write("FooBar, ");
+    } else if (i % 3 == 0) {
+        Console.Write("Foo, ");
     } else {
         Console.Write(i + ", ");
     }
