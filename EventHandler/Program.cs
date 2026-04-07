2b86b6be6fad3a2974a1a19bc59a0bdc28095375
denny <denycam9@gmail.com>
2026-04-07 10:47:26 +0700

    feat: event handler

---
+++ EventHandler/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program {
+    static void Main(string[] args) {
+
+        var oil = new Oil("Pertalite", "calm");
+
+        oil.StatusChanged += SendAnnouncement;
+        oil.StatusChanged += SendPriceAnnouncement;
+        Console.WriteLine("--- Condition Changed ---");
+        oil.Condition = "war";
+    }
+
+    static void SendAnnouncement(object? sender, StatusChangedEventArgs e) {
+        Console.WriteLine($"[BAHLIL]: War begins");
+    }
+
+    static void SendPriceAnnouncement(object? sender, StatusChangedEventArgs e) {
+        Console.WriteLine($"[BAHLIL]: Price will increase 1000");
+    }
+}
+
+public class StatusChangedEventArgs : EventArgs {
+    public string Status { get; }
+
+
+    public StatusChangedEventArgs(string status) {
+        Status = status;
+    }
+}
+
+public class Oil {
+    public string Name { get; set; }
+    private string _condition;
+    private int _price = 10_000;
+
+    public event EventHandler<StatusChangedEventArgs>? StatusChanged;
+
+    public Oil(string name, string condition) {
+        Name = name;
+        _condition = condition;
+    }
+
+    public string Condition {
+        get => _condition;
+        set {
+            if (value == "war") {
+                _condition = "war";
+                _price += 1000;
+                OnStatusChanged(new StatusChangedEventArgs(value));
+            }
+        }
+    }
+
+    protected virtual void OnStatusChanged(StatusChangedEventArgs e) {
+        StatusChanged?.Invoke(this, e);
+    }
+}
