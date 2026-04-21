57836ec5ff3b82fbbfb0d6aad19f1c98f358389b
denny <denycam9@gmail.com>
2026-04-21 11:49:47 +0700

    feat: Threading

---
+++ Threading/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Membuat thread baru
+// Thread thread = new(new ThreadStart(MetodeSaya));
+// thread.Start(); // Mulai eksekusi
+
+// void MetodeSaya() {
+//     Console.WriteLine("Berjalan di thread terpisah");
+// }
+
+// // Menjalankan tugas di ThreadPool
+// ThreadPool.QueueUserWorkItem(state => {
+//     Console.WriteLine("Tugas dari ThreadPool");
+// });
+
+// Thread - low level
+
+Thread thread = new Thread(() => {
+    int result = Compute();
+    // Bagaimana return value?
+});
+
+int Compute() {
+    throw new NotImplementedException();
+}
+
+thread.Start();
+
+// Task - high level dengan return value
+Task<int> task = Task.Run(() => Compute());
+int result = await task;  // Bisa await!
